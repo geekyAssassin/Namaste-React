a16f3d9f9fe86a384fa852b1dd9953640fb40540
Bhavani Sandra <sandrabhavaniprasad@gmail.com>
2023-10-28 17:58:36 +0530

    Episode 1 of Namaste React

---
+++ App.js
@@ -0,0 +1,33 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+/* To create a Nested Structure and siblings(H1 and H2) like below
+<div id = "parent">
+    <div id = "child">
+        <h1>Hi , I am H1 Tag</h1>
+        <h2>Hi , I am H2 Tag</h2>
+    </div>
+    <div id = "child2">
+        <h1>Hi , I am H1 Tag</h1>
+        <h2>Hi , I am H2 Tag</h2>
+    </div>
+</div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hi, I am H1 Tag"),
+    React.createElement("h2", {}, "Hi, I am H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hi, I am H1 Tag"),
+    React.createElement("h2", {}, "Hi, I am H2 Tag"),
+  ]),
+]);
+
+root.render(parent);
